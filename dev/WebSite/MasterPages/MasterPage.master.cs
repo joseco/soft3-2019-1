8449f3314bec058ca9c9ba1526ba49df02bc2a4a
Jose Carlos Gutierrez <joseco48@gmail.com>
2019-04-02 21:36:48 -0400

    Version inicial del proyecto y la base de datos. Se incluyó validacion de las versiones

---
+++ dev/WebSite/MasterPages/MasterPage.master.cs
@@ -0,0 +1,34 @@
+﻿using App.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class MasterPage : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (IsPostBack)
+            return;
+
+        bool isValidVersion = false;
+        try
+        {
+            isValidVersion = VersionUtilities.AppIsValidVersion();                
+        }
+        catch (Exception ex)
+        {
+            Session["ErrorMessage"] = ex.Message;
+            Response.Redirect("~/Error.aspx");
+        }
+
+        if (!isValidVersion)
+        {
+            Session["ErrorMessage"] = "La version de la Aplicacion no es la misma que la version de la Base de Datos.";
+            Response.Redirect("~/Error.aspx");
+        }
+
+    }
+}
